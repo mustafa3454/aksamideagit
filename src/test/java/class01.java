1d9c3fbcd67b3efa175d4fe4defe644ce1e58494
findik gencoglu <mustafakepenek@gmail.com>
2023-02-23 21:56:47 +0100

    v2

--- src/test/java/class01.java
+++ src/test/java/class01.java
@@ -1,5 +1,7 @@
 public class class01 {
     public static void main(String[] args) {
         System.out.println("namaz icin abdes al");
+        System.out.println("namaz  kil  vaktinde");
+
     }
 }
