faebf793952ea8cc97f84d5b3238f26d61c25b6e
findik gencoglu <mustafakepenek@gmail.com>
2023-02-23 22:04:12 +0100

    v5

--- src/test/java/class01.java
+++ src/test/java/class01.java
@@ -2,6 +2,7 @@ public class class01 {
     public static void main(String[] args) {
         System.out.println("namaz icin abdes al");
         System.out.println("namaz  kil  vaktinde");
+        System.out.println("yagmurlu havgitada namaz kil ");
 
     }
 }
